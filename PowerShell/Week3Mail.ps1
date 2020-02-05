b670666dac7bf21e3369466b700a9f900d350ea6
hill3mj <hill3mj@mail.uc.edu>
2020-02-04 19:08:40 -0500

    adding powershell files

---
+++ PowerShell/Week3Mail.ps1
@@ -0,0 +1,21 @@
+﻿function getIP {
+(Get-NetIPAddress).IPAddress | Select-String "192*"
+}
+function getTheDate {
+get-date -DisplayHint date
+}
+
+
+$IP = getIP
+$User = $env:UserName
+$Username = $env:COMPUTERNAME
+$Version = $(get-host).Version.Major
+$Date = getTheDate
+$Body = "This machines IP is $IP. User is $User. Hostname is $Username. Powershell version $Version. Todays date is $Date."
+
+#Write-Host($BODY)
+
+
+Send-MailMessage -To "mic8786@mail.com" -From "smtp@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $Body -SmtpServer smtp.gmail.com -Port 587 -UseSsl -Credential (Get-Credential)
+
+Write-Host("email sent")
---
+++ PowerShell/getIP.ps1
@@ -0,0 +1,7 @@
+﻿function getIP {
+(Get-NetIPAddress).IPAddress | Select-String "192*"
+}
+
+$IP= getIP
+
+write-host("This machine's IP is $IP")
